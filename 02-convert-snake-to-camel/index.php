c48314b6cef72ce54eb4f6acb6dcdc7816c6db05
Thitiphan Saragool <bxnkphn@gmail.com>
2026-02-11 07:16:37 +0700

    chore: add the question within comment

---
+++ 02-convert-snake-to-camel/index.php
@@ -0,0 +1,8 @@
+<?php
+/* โจทย์ข้อที่ 2 (10 คะแนน)
+จงเขียน function convertSnakeToCamel(String $str);
+ที่สามารถแปลงสตริงในรูปแบบ Snake Case เป็น Camel Case  และ Return ผลลัพธิ์เป็น String
+ข้อกำหนด
+-	รับสตริงในรูปแบบ Snake Case เช่น "this_is_a_test" และแปลงให้เป็น Camel Case เช่น "thisIsATest"
+*/
+?>
